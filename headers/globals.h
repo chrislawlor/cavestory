d40de83dc878c2e5ac9f0a5e6d4979506333450d
Chris Lawlor <chris.lawlor@stylecaster.com>
2015-12-18 21:54:11 -0500

    Fix global width variable

--- headers/globals.h
+++ headers/globals.h
@@ -2,7 +2,7 @@
 #define GLOBALS_H
 
 namespace globals {
-    const int SCREEN_WIDTH = 600;
+    const int SCREEN_WIDTH = 640;
     const int SCREEN_HEIGHT = 480;
     const int SPRITE_SCALE = 2;
 }
